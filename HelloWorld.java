a1956dcf2a8c069122199423af8e9fa7118225ed
Vova Roman <vovan.roman7@gmail.com>
2025-08-19 11:33:16 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/* Author @RZFeeser
+   A simple Java app that can be compiled by our GitHub workflow */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+}
